48bc00fc9d1f371bab57a063b82bf17aefd4511a
kevinliu0605 <aidisheng880@gmail.com>
2022-03-15 18:15:08 -0400

    outline update

---
+++ startracker/src/components/Main.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Row, Col } from "antd";
+
+class Main extends Component {
+  render() {
+    return <Row>main</Row>;
+  }
+}
+export default Main;
